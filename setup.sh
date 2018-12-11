578f7039974953b33388f6950061cead1f7af685
Tarun Verghis <tarun.verghis@gmail.com>
2018-12-11 01:28:47 -0600

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Must have at exactly one parameter"
+    exit
+fi
+
+day=$1
+name="$(printf "day_%02d" "$day")"
+
+# Create cargo binary project
+cargo new $name
+
+# Retrieve the day's input from the AOC site
+session="$(cat session.txt)"
+curl --header "Cookie: session="$session"" https://adventofcode.com/2018/day/$day/input > ./$name/input.txt
+
+# Let's go!
+code ./$name/
