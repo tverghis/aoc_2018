31d605eed2cf62a1a37104de930650a0687526da
Tarun Verghis <tarun.verghis@gmail.com>
2018-12-11 09:41:31 -0600

    Add Day 2 Part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
 
     println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
 fn part1(input: &str) -> u32 {
@@ -25,7 +26,7 @@ fn part1(input: &str) -> u32 {
             let m: Vec<_> = line.matches(c).collect();
             has_double = has_double || (m.len() == 2);
             has_triple = has_triple || (m.len() == 3);
-            seen_set.insert(c.clone());
+            seen_set.insert(c);
         }
 
         if has_double {
@@ -37,5 +38,39 @@ fn part1(input: &str) -> u32 {
         }
     }
 
-    return doubles * triples;
+    doubles * triples
+}
+
+fn part2(input: &str) -> String {
+    let mut ids = ("", "");
+
+    for (n, id) in input.lines().enumerate() {
+        for cmp_id in input.lines().skip(n + 1) {
+            // Assume that input lines all have the same length
+            assert_eq!(id.len(), cmp_id.len());
+
+            let mut differences = 0;
+
+            for (n1, c1) in id.chars().enumerate() {
+                if cmp_id.chars().nth(n1).unwrap() != c1 {
+                    differences += 1;
+                }
+
+                if differences > 1 {
+                    break;
+                }
+            }
+
+            if differences == 1 {
+                ids = (id, cmp_id);
+            }
+        }
+    }
+
+    ids.0
+        .chars()
+        .zip(ids.1.chars())
+        .filter(|(c1, c2)| c1 == c2)
+        .map(|(c1, _)| c1)
+        .collect()
 }
