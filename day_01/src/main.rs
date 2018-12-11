ff0ce6d7d8b212a37a62ce1d7b636f7567e767a5
Tarun Verghis <tarun.verghis@gmail.com>
2018-12-11 03:43:16 -0600

    Add Day 1 Part 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::io::{self, Read};
 
 fn main() {
@@ -5,6 +6,7 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
 
     println!("{:?}", part1(&input));
+    println!("{:?}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
@@ -15,4 +17,21 @@ fn part1(input: &str) -> i32 {
     }
 
     sum
+}
+
+fn part2(input: &str) -> i32 {
+    let mut current_freq = 0;
+    let mut seen_set = HashSet::new();
+
+    seen_set.insert(current_freq);
+
+    loop {
+        for line in input.lines() {
+            current_freq += line.parse::<i32>().unwrap();
+            if seen_set.contains(&current_freq) {
+                return current_freq;
+            }
+            seen_set.insert(current_freq);
+        }
+    }
 }
